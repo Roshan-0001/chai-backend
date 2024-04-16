5b8debcbc6cce16a06f746f5db0aa2cdc5b00069
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-04-16 16:32:23 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,12 @@ app.use(express.static("public"))
 
 
 
+//import routers
+
+import userRouter from './routes/user.route.js'
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
 export {app}
